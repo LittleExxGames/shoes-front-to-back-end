a806b388146885d0de975a7b3967b90afd214d23
CJ <52461238+LittleExxGames@users.noreply.github.com>
2024-05-23 11:29:56 -0700

    Added a fetch to get data and had it loop to display the first 3 shoes from the database

--- shoes-front-end/src/App.js
+++ shoes-front-end/src/App.js
@@ -1,14 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+
 
 function App() {
+  
+const [searchTerm, setSearchTerm] = useState('');
+const [shoes, setShoes] = useState([]);
+
+useEffect(() => {
+  fetch('http://localhost:3001/shoes')
+  .then(res => res.json())
+  .then(data => {
+    setShoes([...shoes, ...data]);
+    console.log(data);
+  });
+}, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <input type="text" className="Retrieve"></input>
+        {shoes.map((shoe, index) => (
+        <p key={index}>
+           {shoe.brand} - {shoe.model} - {shoe.size}
         </p>
+      ))}
         <a
           className="App-link"
           href="https://reactjs.org"
